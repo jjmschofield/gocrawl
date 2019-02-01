d1853d52dd47365fa98da1bf22f7f423d684c516
Jack Schofield <jjmschofield@googlemail.com>
2019-02-01 18:57:57 +0000

    Adding a file and stdout writer

---
+++ internal/app/crawl/writers.go
@@ -0,0 +1,52 @@
+package crawl
+
+import (
+	"encoding/json"
+	"github.com/jjmschofield/GoCrawl/internal/app/pages"
+	"log"
+	"os"
+	"sync"
+)
+
+type Writer func(in chan pages.Page, wg *sync.WaitGroup)
+
+func StdoutWriter(in chan pages.Page, wg *sync.WaitGroup){
+	defer wg.Done()
+	wg.Add(1)
+
+	pageEncoder := json.NewEncoder(os.Stdout)
+
+	for page := range in {
+		err := pageEncoder.Encode(page)
+
+		if err != nil {
+			log.Panicln("Can't write entry!")
+		}
+	}
+}
+
+type FileWriter struct {
+	FilePath string
+}
+
+func (w *FileWriter) Write (in chan pages.Page, wg *sync.WaitGroup){
+	defer wg.Done()
+	wg.Add(1)
+
+	jsonlFile, err := os.Create(w.FilePath)
+	defer jsonlFile.Close()
+
+	pageEncoder := json.NewEncoder(jsonlFile)
+
+	if err != nil {
+		log.Panicln("Can't open file to write results to!")
+	}
+
+	for page := range in {
+		err := pageEncoder.Encode(page)
+
+		if err != nil {
+			log.Panicln("Can't write entry!")
+		}
+	}
+}
