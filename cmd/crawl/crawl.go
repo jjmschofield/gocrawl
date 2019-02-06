47129bc54264f138628550d65e65c7dec9149c60
Jack Schofield <jjmschofield@googlemail.com>
2019-02-06 21:03:32 +0000

    Moving cmd/crawl to a sub dir and adding a benchmarking test

--- cmd/crawl/crawl.go
+++ cmd/crawl/crawl.go
@@ -21,15 +21,18 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-
 	start := time.Now()
 
-	crawler := crawl.NewDefaultCrawler(*workerCount, *outFilePath)
-
-	counters := crawler.Crawl(*crawlUrl)
+	counters := Crawl(*crawlUrl, *workerCount, *outFilePath)
 
 	end := time.Now()
 
 	fmt.Printf("Scrape Completed in %v ms \n", (end.UnixNano()-start.UnixNano())/int64(time.Millisecond))
 	fmt.Printf(" Discovered: %v, \n Crawled: %v \n Parallel Crawls Peak: %v \n Scrape Queue Peak: %v \n Processing Peak: %v \n", counters.Discovered.Count(), counters.CrawlComplete.Count(), counters.Crawling.Peak(), counters.CrawlsQueued.Peak(), counters.Processing.Peak())
+}
+
+func Crawl(crawlUrl url.URL, workerCount int, outFilePath string) crawl.Counters {
+	crawler := crawl.NewDefaultPageCrawler(workerCount, outFilePath)
+	counters := crawler.Crawl(crawlUrl)
+	return counters
 }
